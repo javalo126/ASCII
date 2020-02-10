78c51126eec73335c48b298a1fd4a5a468f1e324
Jose <josea2@illinois.edu>
2020-02-10 00:59:23 -0600

    second ascii

--- myfile.java
+++ myfile.java
@@ -1,3 +1,31 @@
+
+public class myFile {
+
+
+    public static void main(String[] args) {
+         BufferedImage image = null;
+
+
+         try {
+                 img = ImageIO.read(new File("/home/jose/tutorial/testImage.jpg"));
+         } catch (IOException e) {
+         }
+
+         // reads test image 
+
+         for ( i
+
+                         // read across entire image, figure out how to replace pixels // 
+
+
+
+
+
+
+    }
+ }
+
+
 import java.io.File; 
 import java.io.IOException; 
 import java.awt.image.BufferedImage; 
