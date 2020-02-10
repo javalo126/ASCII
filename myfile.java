da3bb071bf88aed4f2b14e2dd643ab0e5fa0ecbe
Jose <josea2@illinois.edu>
2020-02-10 00:29:07 -0600

    ascii commit

---
+++ myfile.java
@@ -0,0 +1,34 @@
+import java.io.File; 
+import java.io.IOException; 
+import java.awt.image.BufferedImage; 
+import javax.imageio.ImageIO; 
+
+
+
+
+
+public class myFile { 
+
+
+    public static void main(String[] args) { 
+	 BufferedImage image = null;  
+	 
+
+	 try { 
+		 img = ImageIO.read(new File("/home/jose/tutorial/testImage.jpg"));
+	 } catch (IOException e) { 
+	 } 
+
+	 // reads test image 
+	 
+	 byte[] pixels = ((DataBufferByte) bufferedImage.getRaster();
+
+
+
+
+
+    }
+ }
+
+
+
